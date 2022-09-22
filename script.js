66e45b67e0c23ace88958bba92939be6a8b0cbeb
alysvolatile <alice.g.cooper@gmail.com>
2022-09-22 09:46:37 -0400

    create click to copy function, remove extra consolelog

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let generateButton = document.getElementById('generate')
-console.log(typeof generateButton)
-generateButton.addEventListener('click', randomColor)
+let displayText = document.getElementById('hex-display')
+let copyButton = document.getElementById('copy-to-clipboard')
+
 let randomNumber;
+let generatedColor;
+
+generateButton.addEventListener('click', randomColor)
+copyButton.addEventListener('click', clickToCopy)
 
 function randomColor() {
-    // randomNumber = Math.floor(Math.random() * 256).toString(16)
-    // console.log(randomNumber)
-    // document.getElementById('hex-display').innerText = `${randomNumber}`
+    // create random number and convert to hexCode
+    randomNumber = Math.random().toString(16).substring(2,8)
+    generatedColor = `#${randomNumber}`
+    displayText.value = generatedColor
+    // update background color
+    document.body.style.backgroundColor = (generatedColor)
 }
 
+// initial randomize on pageload
 randomColor()
-//hex display
-document.getElementById('hex-display').innerText = `${randomNumber}`
+//hex display on pageload
+displayText.innerText = generatedColor
+
+// click to copy
+function clickToCopy() {
+    displayText.select();
+    displayText.setSelectionRange(0,99999) // for mobile
+    navigator.clipboard.writeText(generatedColor)
+    alert(`Copied ${generatedColor} to clipboard!`)
+}
+
